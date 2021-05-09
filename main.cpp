11f9faff6a458d83c0ca3be0c508a91df711ee13
Wennim <wennim1234@gmail.com>
2021-05-09 09:06:56 -0700

    add

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "mbed.h"
+#include "function.h"
+#include "stm32l475e_iot01_accelero.h"
+
+using namespace std::chrono;
+
+
+
+
+
+
+void data_xyz();
+
+int main()
+ {
+    
+    selecting();
+    
+    
+}
+
