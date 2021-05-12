8b5423ca9423d605b60091ce9fb87da5ee70902d
Wennim <wennim1234@gmail.com>
2021-05-12 01:47:28 -0700

    add

---
+++ rpc_function.py
@@ -0,0 +1,21 @@
+import serial
+import time
+serdev = '/dev/ttyACM0'
+s = serial.Serial(serdev, 9600)
+
+s.write(bytes("\r", 'UTF-8'))
+line=s.readline() # Read an echo string from mbed terminated with '\n' (putc())
+print(line)
+line=s.readline() # Read an echo string from mbed terminated with '\n' (RPC reply)
+print(line)
+time.sleep(1)
+
+s.write(bytes("/Selecting/run 1\r", 'UTF-8'))
+line=s.readline() # Read an echo string from mbed terminated with '\n' (putc())
+print(line)
+line=s.readline() # Read an echo string from mbed terminated with '\n' (RPC reply)
+print(line)
+time.sleep(1)
+
+
+s.close()
