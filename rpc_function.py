a5552afd906e4af7af73c96eb9c3ba37b170b113
Wennim <wennim1234@gmail.com>
2021-05-12 17:31:44 -0700

    add

--- rpc_function.py
+++ rpc_function.py
@@ -1,5 +1,8 @@
 import serial
 import time
+import paho.mqtt.client as paho
+import time
+
 serdev = '/dev/ttyACM0'
 s = serial.Serial(serdev, 9600)
 
@@ -10,18 +13,19 @@ line=s.readline() # Read an echo string from mbed terminated with '\n' (RPC repl
 print(line)
 time.sleep(1)
 
-s.write(bytes("/selecting/run 1 1\r", 'UTF-8'))
+s.write(bytes("/selecting/run\r", 'UTF-8'))
 line=s.readline() # Read an echo string from mbed terminated with '\n' (putc())
 print(line)
 line=s.readline() # Read an echo string from mbed terminated with '\n' (RPC reply)
 print(line)
-time.sleep(30)
+time.sleep(20)
 
-s.write(bytes("/selecting/run 0 0\r", 'UTF-8'))
+s.write(bytes("/detection/run\r", 'UTF-8'))
 line=s.readline() # Read an echo string from mbed terminated with '\n' (putc())
 print(line)
 line=s.readline() # Read an echo string from mbed terminated with '\n' (RPC reply)
 print(line)
-time.sleep(30)
+time.sleep(10)
+
 
 s.close()
